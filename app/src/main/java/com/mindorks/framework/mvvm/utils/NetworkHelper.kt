fefff06c4af2bf0817789d32ef71bc3caefe35a2
Zeeshan Syed <sdzshn3@gmail.com>
2020-06-22 19:42:00 +0530

    suppressed deprecation warning for ConnectivityManager

--- app/src/main/java/com/mindorks/framework/mvvm/utils/NetworkHelper.kt
+++ app/src/main/java/com/mindorks/framework/mvvm/utils/NetworkHelper.kt
@@ -11,6 +11,7 @@ import javax.inject.Singleton
 @Singleton
 class NetworkHelper @Inject constructor(@ApplicationContext private val context: Context) {
 
+    @Suppress("DEPRECATION")
     fun isNetworkConnected(): Boolean {
         var result = false
         val connectivityManager =
